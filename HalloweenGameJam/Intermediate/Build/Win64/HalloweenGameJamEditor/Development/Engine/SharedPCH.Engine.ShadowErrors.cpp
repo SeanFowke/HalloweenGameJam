094dabb98772e5f20756e66c333332827d24f9e8
SeanFowke <seanfowke@hotmail.com>
2021-10-24 18:53:42 -0500

    Fixed Project

---
+++ HalloweenGameJam/Intermediate/Build/Win64/HalloweenGameJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30136
+#include "C:/Users/SeanF/Desktop/HalloweenGameJam/HalloweenGameJam/HalloweenGameJam/HalloweenGameJam/Intermediate/Build/Win64/HalloweenGameJamEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ HalloweenGameJam/Intermediate/Build/Win64/UE4Editor/Inc/HalloweenGameJam/HalloweenGameJamGameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HALLOWEENGAMEJAM_HalloweenGameJamGameMode_generated_h
+#error "HalloweenGameJamGameMode.generated.h already included, missing '#pragma once' in HalloweenGameJamGameMode.h"
+#endif
+#define HALLOWEENGAMEJAM_HalloweenGameJamGameMode_generated_h
+
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_SPARSE_DATA
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_RPC_WRAPPERS
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAHalloweenGameJamGameMode(); \
+	friend struct Z_Construct_UClass_AHalloweenGameJamGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AHalloweenGameJamGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HalloweenGameJam"), HALLOWEENGAMEJAM_API) \
+	DECLARE_SERIALIZER(AHalloweenGameJamGameMode)
+
+
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAHalloweenGameJamGameMode(); \
+	friend struct Z_Construct_UClass_AHalloweenGameJamGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AHalloweenGameJamGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HalloweenGameJam"), HALLOWEENGAMEJAM_API) \
+	DECLARE_SERIALIZER(AHalloweenGameJamGameMode)
+
+
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	HALLOWEENGAMEJAM_API AHalloweenGameJamGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHalloweenGameJamGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(HALLOWEENGAMEJAM_API, AHalloweenGameJamGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHalloweenGameJamGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	HALLOWEENGAMEJAM_API AHalloweenGameJamGameMode(AHalloweenGameJamGameMode&&); \
+	HALLOWEENGAMEJAM_API AHalloweenGameJamGameMode(const AHalloweenGameJamGameMode&); \
+public:
+
+
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	HALLOWEENGAMEJAM_API AHalloweenGameJamGameMode(AHalloweenGameJamGameMode&&); \
+	HALLOWEENGAMEJAM_API AHalloweenGameJamGameMode(const AHalloweenGameJamGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(HALLOWEENGAMEJAM_API, AHalloweenGameJamGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHalloweenGameJamGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AHalloweenGameJamGameMode)
+
+
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_9_PROLOG
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_SPARSE_DATA \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_RPC_WRAPPERS \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_INCLASS \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_SPARSE_DATA \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HALLOWEENGAMEJAM_API UClass* StaticClass<class AHalloweenGameJamGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HalloweenGameJam_Source_HalloweenGameJam_HalloweenGameJamGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
